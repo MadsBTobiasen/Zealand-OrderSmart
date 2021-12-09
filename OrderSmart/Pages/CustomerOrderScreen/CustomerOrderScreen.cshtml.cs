37f41023fc997f81771ca34a422219f36547aa33
Thyge123 <martintsh25@gmail.com>
2021-12-09 12:49:08 +0100

    Hej!

--- OrderSmart/Pages/CustomerOrderScreen/CustomerOrderScreen.cshtml.cs
+++ OrderSmart/Pages/CustomerOrderScreen/CustomerOrderScreen.cshtml.cs
@@ -1,47 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OrderSmart.Models;
 using OrderSmart.Services.OrderService;
+using System.Collections.Generic;
 
 namespace OrderSmart.Pages.CustomerOrderScreen
 {
     public class CustomerOrderScreenModel : PageModel
     {
-
         private OrderService _orderService;
 
         public List<Order> Orders { get; set; }
-
-        [BindProperty] public int ID { get; set; }
-
-        [BindProperty] public Order.Status OrderStatus { get; set; }
 
         public CustomerOrderScreenModel(OrderService orderService)
         {
             _orderService = orderService;
         }
 
+        /// <summary>
+        /// Gets all orders from the list Orders
+        /// Martin
+        /// </summary>
         public void OnGet()
         {
             Orders = _orderService.GetAllOrders();
         }
-
-        //public IActionResult OnPost()
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return Page();
-        //    }
-
-
-
-        //   // catalog.AddPizza(Pizza);
-        //   // return RedirectToPage("GetAllPizzas");
-        //}
 
     }
 }
